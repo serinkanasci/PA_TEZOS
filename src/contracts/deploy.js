83b1119f8b7fea6f19f7c2d4ec3128f6ce8fae72
Lisa Oulmi <lisa.oulmi77@gmail.com>
2022-04-03 19:03:35 +0200

    feat(userprofilerisk): added userprofilerisk set_user_profile_risk status method in smart contract

--- src/contracts/deploy.js
+++ src/contracts/deploy.js
@@ -1,7 +1,7 @@
 import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
 
-import user_json from './User.json'
+import user_json from './UserProfileRisk.json'
 
 const Tezos = new TezosToolkit('https://rpc.hangzhounet.teztnets.xyz');
 
@@ -10,8 +10,35 @@ const signer = new InMemorySigner(pk);
 Tezos.setProvider({signer: signer })
 console.log(user_json)
 
-let mappingusers = new MichelsonMap();
-let mappingbanned = new MichelsonMap();
+
+
+// Smart contract User
+// let mappingusers = new MichelsonMap();
+// let mappingbanned = new MichelsonMap();
+
+
+// Tezos.contract
+//   .originate({
+//     code: user_json,
+  
+//     storage: {
+//       mapping: mappingusers,
+//       mapping_banned: mappingbanned,
+
+//     },
+//   })
+//   .then((originationOp) => {
+//     console.log(`Waiting for confirmation of origination for ${originationOp.contractAddress}...`);
+//     return originationOp.contract();
+//   })
+//   .then((contract) => {
+//     console.log("Origination completed.");
+//   })
+//   .catch((error) => console.log(error));
+
+// Smart contract UserProfileRisk
+let mappinguser = new MichelsonMap();
+let mappinguserrisk = new MichelsonMap();
 
 
 Tezos.contract
@@ -19,8 +46,8 @@ Tezos.contract
     code: user_json,
   
     storage: {
-      mapping: mappingusers,
-      mapping_banned: mappingbanned,
+      mapping: mappinguser,
+      users_risk: mappinguserrisk,
 
     },
   })
@@ -34,7 +61,8 @@ Tezos.contract
   .catch((error) => console.log(error));
 
 
+  // V1 : https://better-call.dev/hangzhou2net/KT1UmXNi1ga2RbuG4kTyqZLjnHZC6zKtLM5U/storage
 
+  // V2 : https://better-call.dev/hangzhou2net/KT1MZfRT5ryY74qzcuSjbZKUFQC7GRWczKxu/storage
 
-
-
+  // V3 : https://better-call.dev/hangzhou2net/KT1MZfRT5ryY74qzcuSjbZKUFQC7GRWczKxu/storage
