2e75c008e46c8d6121ff3ddec8209f95caf57553
Lisa Oulmi <36499020+OlmLisa@users.noreply.github.com>
2022-07-04 10:39:12 +0200

    Update deploy.js

--- src/contracts/deploy.js
+++ src/contracts/deploy.js
@@ -10,65 +10,6 @@ const signer = new InMemorySigner(pk);
 Tezos.setProvider({signer: signer })
 console.log(user_json)
 
-
-
-// Smart contract User
-// let mappingusers = new MichelsonMap();
-// let mappingbanned = new MichelsonMap();
-
-
-// Tezos.contract
-//   .originate({
-//     code: user_json,
-  
-//     storage: {
-//       mapping: mappingusers,
-//       mapping_banned: mappingbanned,
-
-//     },
-//   })
-//   .then((originationOp) => {
-//     console.log(`Waiting for confirmation of origination for ${originationOp.contractAddress}...`);
-//     return originationOp.contract();
-//   })
-//   .then((contract) => {
-//     console.log("Origination completed.");
-//   })
-//   .catch((error) => console.log(error));
-
-// Smart contract UserProfileRisk
-// let mappinguser = new MichelsonMap();
-// let mappinguserrisk = new MichelsonMap();
-
-
-// Tezos.contract
-//   .originate({
-//     code: user_json,
-  
-//     storage: {
-//       mapping: mappinguser,
-//       users_risk: mappinguserrisk,
-
-//     },
-//   })
-//   .then((originationOp) => {
-//     console.log(`Waiting for confirmation of origination for ${originationOp.contractAddress}...`);
-//     return originationOp.contract();
-//   })
-//   .then((contract) => {
-//     console.log("Origination completed.");
-//   })
-//   .catch((error) => console.log(error));
-
-
-  // V1 : https://better-call.dev/hangzhou2net/KT1UmXNi1ga2RbuG4kTyqZLjnHZC6zKtLM5U/storage
-
-  // V2 : https://better-call.dev/hangzhou2net/KT1MZfRT5ryY74qzcuSjbZKUFQC7GRWczKxu/storage
-
-  // V3 : https://better-call.dev/hangzhou2net/KT1MZfRT5ryY74qzcuSjbZKUFQC7GRWczKxu/storage
-
-// Financing Plan Smart Contract: 
-
 let mappinguser = new MichelsonMap();
 let mappingadmin = new MichelsonMap();
 let mappingagent = new MichelsonMap();
@@ -84,7 +25,7 @@ Tezos.contract
       mapping_user: mappinguser,
       mapping_admin: mappingadmin,
       mapping_agent: mappingagent,
-      main_admin : "tz1LykgAH5r5imY4JJciBuusStBNnWGirkt2",
+      main_admin : "tz1Y2BwKRkh4ZMcGAY3rkdMacKfA1AgbdTyZ",
       nfts: mappingnft,
       balances: mappingbalances,
       usable_fund: 0
@@ -99,28 +40,3 @@ Tezos.contract
     console.log("Origination completed.");
   })
   .catch((error) => console.log(error));
-
-// V1 : https://better-call.dev/hangzhou2net/KT1CXA5i1RKhk8CrVbxbDpYfepAeYFx2RE4u/operations
-
-// V2 : https://better-call.dev/hangzhou2net/KT1D48XhgUTi7ANEAWqo13jrn5Y1LCtSpivZ/interact?entrypoint=createUser
-
-// V3 : https://better-call.dev/hangzhou2net/KT1D48XhgUTi7ANEAWqo13jrn5Y1LCtSpivZ/storage
-
-// V4 : https://better-call.dev/hangzhou2net/KT1Fqf4uHJgUrr61ui8Nakp3xDct64uqxFsN/storage
-
-// Tezos.contract
-//   .originate({
-//     code: user_json,
-  
-//     storage: {
-//       address: "KT1Fqf4uHJgUrr61ui8Nakp3xDct64uqxFsN"
-//     },
-//   })
-//   .then((originationOp) => {
-//     console.log(`Waiting for confirmation of origination for ${originationOp.contractAddress}...`);
-//     return originationOp.contract();
-//   })
-//   .then((contract) => {
-//     console.log("Origination completed.");
-//   })
-//   .catch((error) => console.log(error));
